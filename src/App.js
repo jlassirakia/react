8219c1f75a9ba8eb894ce582c8aba8794fb52946
jlassirakia <jlassirakia1919@gmail.com>
2021-03-13 10:34:59 +0100

    textchange

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Form.Label className="label">Email address</Form.Label>
       <Form.Control type="email" placeholder="Enter email" />
       <Form.Text className="text-muted">
-        We'll never share your email with anyone else.
+        We'll never rakia jlassi email with anyone else.
       </Form.Text>
     </Form.Group>
   
